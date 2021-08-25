fed1931d1b684b852f9bbd1c128f0b2362c22549
Heitor Chang <heitor.chang@tempook.com>
2021-08-24 21:04:43 -0300

    procon prices

--- src/price_list.clj
+++ src/price_list.clj
@@ -3,40 +3,46 @@
    :a-test 12 ;kg
 
    :bacon 50 ; /kg
-   :beans 8 ; /kg
+   :beans 8 ; /kg Procon: 6.93
    :bell-pepper 12 ; /kg, green
    :bologna 20 ; /kg
    :broccoli 50 ; /kg
    :butter (/ 9 0.2) ; /kg
-   :canned-tomatoes 7 ; /400g
+   :canned-tomatoes (/ 7 0.4) ; /kg
    :carrot 3.5 ; /kg
    :chicken-breast 24 ; /kg, breast, boneless thigh
+   :condensed-milk (/ 5.75 0.395) ; /kg
    :corn-oil (/ 13.99 0.9) ; /l
    :cornstarch (/ 11 0.5) ; /kg
-   :egg (/ 16 20) ; /unit
-   :flour 3.8 ; /kg, wheat
-   :garlic 27 ; /kg
-   :ground-beef 32 ; /kg
-   :ham 28 ; /kg
+   :egg (/ 16 20) ; /unit Procon: 0.70
+   :flour 3.8 ; /kg, wheat Procon: 3.84
+   :garlic 27 ; /kg Procon: 29.56
+   :ground-beef 32 ; /kg Procon: 36.10
+   :ham 28 ; /kg Procon: 28.64
+   :heavy-cream (/ 6.07 0.3) ; /kg
    :hondashi (/ 40.0 0.5) ; /kg
    :japanese-rice (/ 35.5 5) ; /kg
+   :lime 2.99 ; /kg
+   :margarine (/ 2.86 0.25) ; /kg Procon: 11.44
+   :milk 5.39 ; /l Procon: 3.95
    :miso (/ 15 0.5) ; /kg
-   :mozzarella 40 ; /kg
+   :mozzarella 40 ; /kg Procon: 43.04
    :olive-oil (/ 28 0.5) ; /l
-   :onion 3.2 ; /kg
+   :onion 3.2 ; /kg Procon: 3.26
    :parmesan-cheese (/ 22.5 0.245) ; /kg
-   :pasta (/ 6.79 0.5) ; /kg
-   :pork 30 ; /kg, loin
-   :potato 4 ; /kg
+   :pasta (/ 6.79 0.5) ; /kg Procon: 5.78
+   :pork-loin 30 ; /kg
+   :potato 4 ; /kg Procon: 3.15
    :radish 3 ; /kg   
-   :rice (/ 25 5) ; /kg
-   :sliced-bread 13 ; /kg
-   :steak 42 ; /kg, alcatra
-   :sugar 3.2 ; /kg
-   :sweet-potato 3.99 ; /kg
+   :rice (/ 25 5) ; /kg Procon: 4.11, 20.56 for 5kg
+   :sliced-bread 13 ; /kg Procon: 10.76
+   :steak 42 ; /kg, alcatra Procon: 44.86
+   :sugar 3.2 ; /kg Procon: 3.45
+   :sweet-potato 2.99 ; /kg
    :tomato 8 ; /kg
-   :tomato-paste (/ 3 0.120) ; /kg
-   :wakame (/ 9 0.1) ; kg
+   :tomato-paste (/ 3 0.120) ; /kg Procon: 13.47
+   :wakame (/ 9 0.1) ; /kg
    :white-mushroom (/ 13.4 0.2) ; /kg
+   :whole-chicken 9 ; /kg Procon: 9.62
    :wine (/ 29 0.750) ; /l
    })
